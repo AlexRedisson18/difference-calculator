82af8117c34d931ddce3ab0899c3f66a1cf3072b
AlexRedisson18 <alexandr_kitchenko@mail.ru>
2019-02-14 23:04:27 +0600

    reserve copy

--- src/render.js
+++ src/render.js
@@ -1,8 +1,18 @@
 import _ from 'lodash';
 
 const render = (ast, depth = 1) => {
+const stringify = (value, depth) => {
+  if (_.isObject(value)) {
+    const space = ' '.repeat(depth * 2);
+    const result = _.keys(value).map(key => `${space}  ${key}: ${value[key]}`).join('\n');
+    return `{\n${result}\n${space}}`;
+  }
+  return value;
+};
+
+const render = (ast, depth = 1) => {
+  const space = ' '.repeat(depth * 2);
   const result = ast.map((elem) => {
-    const space = '  '.repeat(depth);
     const {
       key,
       type,
@@ -13,21 +23,21 @@ const render = (ast, depth = 1) => {
 
     switch (type) {
       case 'nested':
-        return `${space}  ${key}: ${render(children, depth + 2)}`;
+        return `${space}  ${key}: ${render(children, depth + 1)}`;
       case 'added':
-        return `${space}+ ${key}: ${valueAfter}`;
+        return `${space}+ ${key}: ${stringify(valueAfter, depth + 1)}`;
       case 'unchanged':
-        return `${space}  ${key}: ${valueAfter}`;
+        return `${space}  ${key}: ${stringify(valueAfter, depth + 1)}`;
       case 'changed':
-        return [`${space}+ ${key}: ${valueAfter}`, `${space}- ${key}: ${valueBefore}`];
+        return [`${space}+ ${key}: ${stringify(valueAfter, depth + 1)}`, `${space}- ${key}: ${stringify(valueBefore, depth + 1)}`];
       case 'deleted':
-        return `${space}- ${key}: ${valueBefore}`;
+        return `${space}- ${key}: ${stringify(valueBefore, depth + 1)}`;
       default:
         return console.log('Current type is not defined');
     }
   });
-  const rendered = _.flatten(result).sort((a, b) => a - b).join('\n');
-  return `{\n${rendered}\n}`;
+  const resultToStr = _.flatten(result).join('\n');
+  return `{\n${resultToStr}\n${space}}`;
 };
 
 export default render;
@@ -70,28 +80,37 @@ export default render;
 
 {
     common: {
-        setting1: Value 1
-      - setting2: 200
-      + setting3: [object Object]
-      - setting3: true
-        setting6: {
-            key: value
-          + ops: vops
-}
-      + follow: false
-      + setting4: blah blah
-      + setting5: [object Object]
-}
+      setting1: Value 1
+    - setting2: 200
+    + setting3: {
+        key: value
+      }
+    - setting3: true
+      setting6: {
+        key: value
+      + ops: vops
+      }
+    + follow: false
+    + setting4: blah blah
+    + setting5: {
+        key5: value5
+      }
+    }
     group1: {
-      + baz: bars
-      - baz: bas
-        foo: bar
-      + nest: str
-      - nest: [object Object]
-}
-  - group2: [object Object]
-  + group3: [object Object]
-}
-
+    + baz: bars
+    - baz: bas
+      foo: bar
+    + nest: str
+    - nest: {
+        key: value
+      }
+    }
+  - group2: {
+      abc: 12345
+    }
+  + group3: {
+      fee: 100500
+    }
+  }
 
 */
